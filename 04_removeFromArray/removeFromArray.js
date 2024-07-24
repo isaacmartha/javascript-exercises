3fea54d2a1f3eb2fac3ecd418e2aeb86689caf9b
isaacmartha <isaaceditar@gmail.com>
2024-07-24 15:52:08 -0300

    Cleaned the code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,13 +2,10 @@ function removeFromArray(array, ...valuesToRemove) {
     return array.filter(item => !valuesToRemove.includes(item));
 }
 
-// Exemplo de uso
 let myArray = [1, 2, 3, 4];
 myArray = removeFromArray(myArray, 3);
 
 console.log(myArray); 
-
-
 
 // Do not edit below this line
 module.exports = removeFromArray;
