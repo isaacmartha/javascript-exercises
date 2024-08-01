ce79af2f70b591f8e50d29c4504042e1c3e8b24e
isaacmartha <isaaceditar@gmail.com>
2024-08-01 16:34:04 -0300

    Made exercise 8 and 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+ var reg = /[\W_]/g;
+ str = str.toLowerCase().replace(reg, "");
 
+ var reversed = str.split("").reverse().join("")
+ if (reversed === str) return true;
+
+ return false;
 };
 
 // Do not edit below this line
